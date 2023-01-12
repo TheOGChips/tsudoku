b8b005818435de7912e451be17133ea727625561
TheOGChips <swindell.christian.g@gmail.com>
2023-01-11 20:08:36 -0600

    Number of completed puzzles can now be displayed

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,18 @@
 #include "sudoku.hpp"
 #include <ncurses.h>
 #include <filesystem>   //filesystem::create_directory, filesystem::exists
-#include <fstream>      //std::ofstream
+#include <fstream>      //std::ofstream, std::ifstream
+#include <sstream>
 
+#undef KEY_ENTER
+const uint8_t KEY_ENTER = 10;
+
+string HOME = getenv("HOME"),
+           dir = HOME + "/.tsudoku",
+           completed = dir + "/completed_puzzles.txt";
+           
 void create_dir ();
+void display_completed_puzzles ();
 
 int main ()
 {
@@ -17,8 +26,10 @@ int main ()
         Sudoku puzzle (true);  //initialize a sudoku puzzle
         puzzle.start_game();
     }
-    else if (opt == options::RESUME_GAME);
-    else;
+    else if (opt == options::RESUME_GAME);  //TODO
+    else {  //TODO
+        display_completed_puzzles();
+    }
     
     clear();
     printw("sizeof(Sudoku): %lu B\n", sizeof(Sudoku));
@@ -30,9 +41,6 @@ int main ()
 
 void create_dir () {
     using namespace std::filesystem;
-    string HOME = getenv("HOME"),
-           dir = HOME + "/.tsudoku",
-           completed = dir + "/completed_puzzles.txt";
     create_directory(dir.c_str());
     
     if (not exists(completed.c_str())) {
@@ -41,4 +49,26 @@ void create_dir () {
         outfile << 0 << endl;
         outfile.close();
     }
+}
+
+void display_completed_puzzles () {
+    uint8_t y_max,
+            x_max;
+    uint64_t num_completed;
+    
+    ifstream infile;
+    infile.open(completed.c_str());
+    infile >> num_completed;
+    infile.close();
+    
+    stringstream sstr;
+    sstr << "Completed Sudoku puzzles: " << num_completed;
+    string str = "Press Enter to continue";
+    getmaxyx(stdscr, y_max, x_max);
+    clear();
+    mvprintw(y_max/2, x_max/2 - sstr.str().size()/2, sstr.str().c_str());
+    mvprintw(y_max/2 + 2, x_max/2 - str.size()/2, str.c_str());
+    refresh();
+    //getch();
+    while (getch() != KEY_ENTER);
 }
