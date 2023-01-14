17cd73176aab306b5e3d98816072fb7040a6b72e
TheOGChips <swindell.christian.g@gmail.com>
2023-01-13 22:55:13 -0600

    Cleaned up the Menu class

--- include/Menu.hpp
+++ include/Menu.hpp
@@ -3,23 +3,38 @@
 
 #include <cstdint>
 #include <string>
+#include "values.hpp"
 
 enum class main_options { NEW_GAME, RESUME_GAME, SHOW_STATS };
 enum class in_game_options { RULES, MANUAL, SAVE_GAME, NONE };
 enum class menu_type { MAIN, IN_GAME };
 
-//TODO: This class would be a good instance to use polymorphism (I think)
+/*
+ * TODO: This class would be a good instance to use polymorphism (look up virtual functions). Look
+ *       into this next.
+ */
 class Menu {
     private:
         menu_type type;
-        const uint8_t IN_GAME_MENU_TITLE_SPACING = 1,   //y-axis, blank lines below menu title
-                      IN_GAME_MENU_DISPLAY_SPACING = 90;    //x-axis, columns for display area
-        void display_main_menu (main_options, uint8_t, uint8_t);
-        void display_in_game_menu (in_game_options, uint8_t, uint8_t);
-        void clear (uint8_t, uint8_t);
-        void display_rules (uint8_t, uint8_t);
-        void display_manual (uint8_t, uint8_t);
-        void screen_reader (std::string, uint8_t&, uint8_t, uint8_t);
+        const uint8_t TOP_PADDING = ORIGINy,    //blank space on all the edges of the window
+                      LEFT_PADDING = ORIGINx,
+                      BOTTOM_PADDING = TOP_PADDING,
+                      RIGHT_PADDING = LEFT_PADDING,
+                      PUZZLE_SPACE = 29,        //space (x and y) for the display puzzle
+                      VERTICAL_DIVIDER = 4,     //blank space between display puzzle and in-game menu
+                      RESULT_MSG_SPACE = 3,     //space for the evaluation result to appear
+                      IN_GAME_MENU_LEFT_EDGE = LEFT_PADDING + PUZZLE_SPACE + VERTICAL_DIVIDER,
+                      IN_GAME_MENU_TITLE_SPACING = 1,   //y-axis, blank lines below menu title
+                      IN_GAME_MENU_DISPLAY_SPACING = 90,    //x-axis, columns for display area
+                      WINDOW_YMIN = TOP_PADDING + PUZZLE_SPACE + RESULT_MSG_SPACE + BOTTOM_PADDING,
+                      WINDOW_XMIN = LEFT_PADDING + PUZZLE_SPACE + VERTICAL_DIVIDER +
+                                    IN_GAME_MENU_DISPLAY_SPACING + RIGHT_PADDING;
+        void display_main_menu (const uint8_t, const uint8_t, main_options);
+        void display_in_game_menu (const uint8_t, const uint8_t, in_game_options);
+        void clear (const uint8_t, const uint8_t);
+        void display_rules (const uint8_t, const uint8_t);
+        void display_manual (const uint8_t, const uint8_t);
+        void screen_reader (const uint8_t, const uint8_t, std::string, uint8_t&);
         
     public:
         Menu (const menu_type);
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -2,7 +2,7 @@
 #include "Menu.hpp"
 #include "colors.hpp"
 #include <sstream>
-#include "values.hpp"
+#include <map>
 
 Menu::Menu (const menu_type type) {
     this->type = type;
@@ -25,83 +25,54 @@ Menu::~Menu () {
     }
 }
 
-void Menu::display_main_menu (main_options opt, uint8_t y, uint8_t x) {
-    string opt1 = "New Game",
-           opt2 = "Resume Game",
-           opt3 = "Show # Finished Games";
+void Menu::display_main_menu (const uint8_t Y, const uint8_t X, main_options opt) {
+    const string TITLE = "MAIN MENU";
+    const uint8_t Y_CENTER = Y / 2,
+                  X_CENTER = X/2 - TITLE.size()/2,
+                  NUM_OPTS = 3;
+    const string OPT[NUM_OPTS] = { "New Game", "Resume Game", "Show # Finished Games" };
+    map<uint8_t, main_options> opt_map;
+    opt_map[0] = main_options::NEW_GAME;
+    opt_map[1] = main_options::RESUME_GAME;
+    opt_map[2] = main_options::SHOW_STATS;
            
-    mvprintw(y/2 - 2, x/2 - 5, "MAIN MENU");
-    if (opt == main_options::NEW_GAME) {
-        attron(COLOR_PAIR(MENU_SELECTION));
-        mvprintw(y/2,     x/2 - 5, opt1.c_str());
-        attroff(COLOR_PAIR(MENU_SELECTION));
-        mvprintw(y/2 + 1, x/2 - 5, opt2.c_str());
-        mvprintw(y/2 + 2, x/2 - 5, opt3.c_str());
+    mvprintw(Y_CENTER - 2, X_CENTER, TITLE.c_str());
+    for (uint8_t i = 0; i < NUM_OPTS; i++) {
+        if (opt == opt_map[i]) attron(COLOR_PAIR(MENU_SELECTION));
+        mvprintw(Y_CENTER + i, X_CENTER, OPT[i].c_str());
+        if (opt == opt_map[i]) attroff(COLOR_PAIR(MENU_SELECTION));
     }
-    else if (opt == main_options::RESUME_GAME) {
-        mvprintw(y/2,     x/2 - 5, opt1.c_str());
-        attron(COLOR_PAIR(MENU_SELECTION));
-        mvprintw(y/2 + 1, x/2 - 5, opt2.c_str());
-        attroff(COLOR_PAIR(MENU_SELECTION));
-        mvprintw(y/2 + 2, x/2 - 5, opt3.c_str());
-    }
-    else {
-        mvprintw(y/2,     x/2 - 5, opt1.c_str());
-        mvprintw(y/2 + 1, x/2 - 5, opt2.c_str());
-        attron(COLOR_PAIR(MENU_SELECTION));
-        mvprintw(y/2 + 2, x/2 - 5, opt3.c_str());
-        attroff(COLOR_PAIR(MENU_SELECTION));
-    }
-}
-
-void Menu::display_in_game_menu (in_game_options opt, uint8_t y, uint8_t x) {
-    string opt1 = "View the rules of sudoku",
-           opt2 = "See game manual",
-           opt3 = "Save current game";
-           
-    mvprintw(y, x, "IN-GAME MENU");
-    if (opt == in_game_options::RULES) {
-        attron(COLOR_PAIR(MENU_SELECTION));
-        mvprintw(y + IN_GAME_MENU_TITLE_SPACING + 1, x, opt1.c_str());
-        attroff(COLOR_PAIR(MENU_SELECTION));
-        mvprintw(y + IN_GAME_MENU_TITLE_SPACING + 2, x, opt2.c_str());
-        mvprintw(y + IN_GAME_MENU_TITLE_SPACING + 3, x, opt3.c_str());
-    }
-    else if (opt == in_game_options::MANUAL) {
-        mvprintw(y + IN_GAME_MENU_TITLE_SPACING + 1, x, opt1.c_str());
-        attron(COLOR_PAIR(MENU_SELECTION));
-        mvprintw(y + IN_GAME_MENU_TITLE_SPACING + 2, x, opt2.c_str());
-        attroff(COLOR_PAIR(MENU_SELECTION));
-        mvprintw(y + IN_GAME_MENU_TITLE_SPACING + 3, x, opt3.c_str());
-    }
-    else if (opt == in_game_options::SAVE_GAME) {
-        mvprintw(y + IN_GAME_MENU_TITLE_SPACING + 1, x, opt1.c_str());
-        mvprintw(y + IN_GAME_MENU_TITLE_SPACING + 2, x, opt2.c_str());
-        attron(COLOR_PAIR(MENU_SELECTION));
-        mvprintw(y + IN_GAME_MENU_TITLE_SPACING + 3, x, opt3.c_str());
-        attroff(COLOR_PAIR(MENU_SELECTION));
-    }
-    else {
-        mvprintw(y + IN_GAME_MENU_TITLE_SPACING + 1, x, opt1.c_str());
-        mvprintw(y + IN_GAME_MENU_TITLE_SPACING + 2, x, opt2.c_str());
-        mvprintw(y + IN_GAME_MENU_TITLE_SPACING + 3, x, opt3.c_str());
-    }
-    //Options
-    //Sudoku rules
-    //Game instructions
-    //Save game
     refresh();
 }
 
-void Menu::clear (uint8_t y_edge, uint8_t x_edge) {
-    uint8_t display_line = 5;
-    for (uint8_t y = y_edge + IN_GAME_MENU_TITLE_SPACING + display_line; y < getmaxy(stdscr); y++) {
-        move(y, x_edge);
+void Menu::display_in_game_menu (const uint8_t Y_EDGE, const uint8_t X_EDGE, in_game_options opt) {
+    const uint8_t NUM_OPTS = 3;
+    const string TITLE = "IN-GAME MENU",
+                 OPT[NUM_OPTS] = { "View the rules of sudoku",
+                                   "See game manual", 
+                                   "Save current game"};
+    map<uint8_t, in_game_options> opt_map;
+    opt_map[0] = in_game_options::RULES;
+    opt_map[1] = in_game_options::MANUAL;
+    opt_map[2] = in_game_options::SAVE_GAME;
+           
+    mvprintw(Y_EDGE, X_EDGE, TITLE.c_str());
+    for (uint8_t i = 0; i < NUM_OPTS; i++) {
+        if (opt == opt_map[i]) attron(COLOR_PAIR(MENU_SELECTION));
+        mvprintw(Y_EDGE + IN_GAME_MENU_TITLE_SPACING + i + 1, X_EDGE, OPT[i].c_str());
+        if (opt == opt_map[i]) attroff(COLOR_PAIR(MENU_SELECTION));
+    }
+    refresh();
+}
+
+void Menu::clear (const uint8_t Y_EDGE, const uint8_t X_EDGE) {
+    for (uint8_t y = Y_EDGE + IN_GAME_MENU_TITLE_SPACING + 5; y < getmaxy(stdscr); y++) {
+        move(y, X_EDGE);
         clrtoeol();
     }
 }
 
-void Menu::display_rules (uint8_t y_edge, uint8_t x_edge) {
+void Menu::display_rules (const uint8_t Y_EDGE, const uint8_t X_EDGE) {
     const string TITLE = "RULES FOR PLAYING SUDOKU",
                  RULES_INTRO = string("Sudoku is a puzzle game using the numbers 1-9. The ") +
                                "puzzle board is a 9x9 grid that can be broken up evenly in 3 " +
@@ -112,54 +83,59 @@ void Menu::display_rules (uint8_t y_edge, uint8_t x_edge) {
                  RULES_COLUMNS = "2. Each column can only contain one each of the numbers 1-9",
                  RULES_SUBMATRIX = "3. Each sub-grid can only contain one each of the numbers 1-9";
                  
-    uint8_t display_line = 5;
     const uint8_t NUM_RULES = 4;
-    string rules[NUM_RULES] = { RULES_INTRO, RULES_ROWS, RULES_COLUMNS, RULES_SUBMATRIX };
+    string rules_text[NUM_RULES] = { RULES_INTRO, RULES_ROWS, RULES_COLUMNS, RULES_SUBMATRIX };
+    uint8_t display_offset = 5;
     
-    mvprintw(y_edge + IN_GAME_MENU_TITLE_SPACING + display_line++, x_edge, TITLE.c_str());
+    mvprintw(Y_EDGE + IN_GAME_MENU_TITLE_SPACING + display_offset++, X_EDGE, TITLE.c_str());
     for (uint8_t i = 0; i < NUM_RULES; i++) {
-        display_line++;
-        screen_reader(rules[i], display_line, y_edge, x_edge);
+        display_offset++;
+        screen_reader(Y_EDGE, X_EDGE, rules_text[i], display_offset);
     }
 }
 
-void Menu::display_manual (uint8_t y_edge, uint8_t x_edge) {
+void Menu::display_manual (const uint8_t Y_EDGE, const uint8_t X_EDGE) {
     const string TITLE = "TSUDOKU GAME MANUAL",
                  MANUAL_INTRO = string("Red numbers are hints provided for you when the puzzle ") +
-                 "has been generated. The number of hints present corresponds to the difficulty " +
-                 "level you have chosen. Cells with white '?' symbols are empty cells which you " +
-                 "must solve for to complete the puzzle. To enter a number 1-9 into an empty " +
-                 "cell, simply move the cursor over to an empty cell and type the number. The " +
-                 "'?' symbol will be replaced with the number you entered, which will be green " +
-                 "in color. To remove a number from one of these cells, move the cursor over the " +
-                 "cell and press either the Backspace or Delete keys; the green number will be " +
-                 "replaced with a '?' symbol again. The eight blank cells surrounding each " +
-                 "sudoku puzzle cell are available as a note-taking area when analyzing what " +
-                 "numbers should go in that particular cell; numbers entered in these cells will " +
-                 "appear yellow in color. Numbers in these cells can also be removed by pressing " +
-                 "either the Backspace or Delete keys while the cursor is over the cell. You " +
-                 "cannot enter anything in the note-taking cells surrounding puzzle cells with " +
-                 "red numbers. BEWARE: Entering a number in a '?' occupied cell will also erase " +
-                 "your notes in the eight surrounding cells. This action cannot be undone.",
+                                "has been generated. The number of hints present corresponds to " +
+                                "the difficulty level you have chosen. Cells with white '?' " +
+                                "symbols are empty cells which you must solve for to complete " +
+                                "the puzzle. To enter a number 1-9 into an empty cell, simply " +
+                                "move the cursor over to an empty cell and type the number. The " +
+                                "'?' symbol will be replaced with the number you entered, which " +
+                                "will be green in color. To remove a number from one of these " +
+                                "cells, move the cursor over the cell and press either the " +
+                                "Backspace or Delete keys; the green number will be replaced " +
+                                "with a '?' symbol again. The eight blank cells surrounding each " +
+                                "sudoku puzzle cell are available as a note-taking area when " +
+                                "analyzing what numbers should go in that particular cell; " +
+                                "numbers entered in these cells will appear yellow in color. " +
+                                "Numbers in these cells can also be removed by pressing either " +
+                                "the Backspace or Delete keys while the cursor is over the cell. " +
+                                "You cannot enter anything in the note-taking cells surrounding " +
+                                "puzzle cells with red numbers. BEWARE: Entering a number in a " +
+                                "'?' occupied cell will also erase your notes in the eight " +
+                                "surrounding cells. This action cannot be undone.",
                  MANUAL_M = "m/M -> Enter/Exit the in-game manual",
                  MANUAL_Q = "q/Q -> Quit the game without saving",
                  MANUAL_DIR_KEYS = "Up, Down, Left, Right -> Navigate the sudoku board",
                  MANUAL_NUM = "1-9 -> Places number in cell highlighted by cursor",
                  MANUAL_ENTER = "Enter -> Evaluate the puzzle. Analysis will appear below puzzle.";
                  
-    uint8_t display_line = 5;
     const uint8_t NUM_MANUAL = 6;
-    string manual[NUM_MANUAL] = { MANUAL_INTRO, MANUAL_M, MANUAL_Q, MANUAL_DIR_KEYS, MANUAL_NUM,
-                                  MANUAL_ENTER };
-                                  
-    mvprintw(y_edge + IN_GAME_MENU_TITLE_SPACING + display_line++, x_edge, TITLE.c_str());
+    string manual_text[NUM_MANUAL] = { MANUAL_INTRO, MANUAL_M, MANUAL_Q, MANUAL_DIR_KEYS,
+                                       MANUAL_NUM, MANUAL_ENTER };
+    uint8_t display_offset = 5;
+    
+    mvprintw(Y_EDGE + IN_GAME_MENU_TITLE_SPACING + display_offset++, X_EDGE, TITLE.c_str());
     for (uint8_t i = 0; i < NUM_MANUAL; i++) {
-        display_line++;
-        screen_reader(manual[i], display_line, y_edge, x_edge);
+        display_offset++;
+        screen_reader(Y_EDGE, X_EDGE, manual_text[i], display_offset);
     }
 }
 
-void Menu::screen_reader (string str, uint8_t& display_line, uint8_t y_edge, uint8_t x_edge) {
+void Menu::screen_reader (const uint8_t Y_EDGE, const uint8_t X_EDGE, string str,
+                          uint8_t& display_offset) {
     string display_str;
     while (not str.empty()) {
         size_t space_pos = str.find_first_of(' ');
@@ -167,7 +143,7 @@ void Menu::screen_reader (string str, uint8_t& display_line, uint8_t y_edge, uin
         if (space_pos == string::npos) {
             display_str += str;
             str.clear();
-            mvprintw(y_edge + IN_GAME_MENU_TITLE_SPACING + display_line++, x_edge,
+            mvprintw(Y_EDGE + IN_GAME_MENU_TITLE_SPACING + display_offset++, X_EDGE,
                      display_str.c_str());
         }
         else if (display_str.size() + space_pos + 1 < IN_GAME_MENU_DISPLAY_SPACING - 3) {
@@ -175,7 +151,7 @@ void Menu::screen_reader (string str, uint8_t& display_line, uint8_t y_edge, uin
             str = str.substr(space_pos + 1);
         }
         else {
-            mvprintw(y_edge + IN_GAME_MENU_TITLE_SPACING + display_line++, x_edge,
+            mvprintw(Y_EDGE + IN_GAME_MENU_TITLE_SPACING + display_offset++, X_EDGE,
                      display_str.c_str());
             display_str.clear();
         }
@@ -185,14 +161,9 @@ void Menu::screen_reader (string str, uint8_t& display_line, uint8_t y_edge, uin
 
 main_options Menu::main_menu () {
     uint8_t x_max,
-            y_max,
-            puzzle_space = 27,
-            result_msg_space = 5,
-            x_right_padding = 1,
-            y_min = ORIGINy * 2 + puzzle_space + result_msg_space,
-            x_min = ORIGINx * 2 + puzzle_space + IN_GAME_MENU_DISPLAY_SPACING + x_right_padding;
+            y_max;
     getmaxyx(stdscr, y_max, x_max);
-    while (y_max < y_min or x_max < x_min) {
+    while (y_max < WINDOW_YMIN or x_max < WINDOW_XMIN) {
         uint8_t x_curr,
                 y_curr;
         ::clear();
@@ -200,7 +171,7 @@ main_options Menu::main_menu () {
                msg4 = "Resize the terminal window and press Enter twice to continue";
         stringstream msg2,
                      msg3;
-        msg2 << "Required dimensions: " << x_min+0 << " x " << y_min+0;
+        msg2 << "Required dimensions: " << WINDOW_XMIN+0 << " x " << WINDOW_YMIN+0;
         msg3 << "Current dimensions: " << x_max+0 << " x " << y_max+0;
         mvprintw(y_max/2,     x_max/2 - msg1.size()/2,       msg1.c_str());
         mvprintw(y_max/2 + 2, x_max/2 - msg2.str().size()/2, msg2.str().c_str());
@@ -208,11 +179,10 @@ main_options Menu::main_menu () {
         mvprintw(y_max/2 + 5, x_max/2 - msg4.size()/2,       msg4.c_str());
         refresh();
         getmaxyx(stdscr, y_max, x_max);
-        while (getch() != KEY_ENTER);
-    }
+        while (getch() != KEY_ENTER);   //NOTE: For some reason, the Enter key needs to be pressed
+    }                                   //      twice here
     ::clear();
     
-    //const uint8_t KEY_ENTER = 10;
     string msg1 = "The window is now an appropriate size",
            msg2 = "Press Enter to continue";
     mvprintw(y_max/2,     x_max/2 - msg1.size()/2, msg1.c_str());
@@ -223,17 +193,16 @@ main_options Menu::main_menu () {
     
     curs_set(0);
     main_options opt = main_options::NEW_GAME;
-    display_main_menu(opt, y_max, x_max);
+    display_main_menu(y_max, x_max, opt);
     
     uint16_t input;
     do {
-        refresh();
         input = getch();
         switch (input) {
-            case KEY_DOWN:  display_main_menu(++opt, y_max, x_max);
+            case KEY_DOWN:  display_main_menu(y_max, x_max, ++opt);
                             break;
                             
-            case KEY_UP:    display_main_menu(--opt, y_max, x_max);
+            case KEY_UP:    display_main_menu(y_max, x_max, --opt);
                             break;
                 
             default:;
@@ -244,17 +213,13 @@ main_options Menu::main_menu () {
     return opt;
 }
 
-void Menu::in_game_menu () {
-    uint8_t y_edge = ORIGINy,
-            in_game_menu_edge = 5,
-            x_edge = ORIGINx * 2 + 27 + in_game_menu_edge;
-    in_game_options opt = in_game_options::RULES;
-    
+void Menu::in_game_menu () {    
     curs_set(0);
+    in_game_options opt = in_game_options::RULES;
     uint16_t input;
     do {
         refresh();
-        display_in_game_menu(opt, y_edge, x_edge);
+        display_in_game_menu(TOP_PADDING, IN_GAME_MENU_LEFT_EDGE, opt);
         input = getch();
         switch (input) {
             case KEY_DOWN:  ++opt;
@@ -265,12 +230,12 @@ void Menu::in_game_menu () {
                             
             //TODO: Case for Enter key
             case KEY_ENTER: if (opt == in_game_options::RULES) {
-                                clear(y_edge, x_edge);
-                                display_rules(y_edge, x_edge);
+                                clear(TOP_PADDING, IN_GAME_MENU_LEFT_EDGE);
+                                display_rules(TOP_PADDING, IN_GAME_MENU_LEFT_EDGE);
                             }
                             else if (opt == in_game_options::MANUAL) {
-                                clear(y_edge, x_edge);
-                                display_manual(y_edge, x_edge);
+                                clear(TOP_PADDING, IN_GAME_MENU_LEFT_EDGE);
+                                display_manual(TOP_PADDING, IN_GAME_MENU_LEFT_EDGE);
                             }
                             else {  //NOTE: opt will never be NONE based on this logic
                                 //TODO: Saving games
@@ -278,7 +243,7 @@ void Menu::in_game_menu () {
             default:;
         }
     } while (tolower(input) != 'm');
-    display_in_game_menu(in_game_options::NONE, y_edge, x_edge);
+    display_in_game_menu(TOP_PADDING, IN_GAME_MENU_LEFT_EDGE, in_game_options::NONE);
     curs_set(1);
 }
 
