30e24627a1e75b02a18362af6c150deaae215013
TheOGChips <swindell.christian.g@gmail.com>
2023-01-27 12:31:22 -0600

    Changed color codes to an enum

--- include/colors.hpp
+++ include/colors.hpp
@@ -3,12 +3,7 @@
 
 #include <string>   //std::string
 
-//TODO: Make this an enum
-const uint8_t UNKNOWN = 1,
-              GIVEN = 2,
-              CANDIDATES = 3,
-              GUESS = 4,
-              MENU_SELECTION = 5;
+enum colors { UNKNOWN = 1, GIVEN, CANDIDATES, GUESS, MENU_SELECTION};
 const std::string color_code[5] = { "n", "u", "r", "y", "g"};
 
 #endif
