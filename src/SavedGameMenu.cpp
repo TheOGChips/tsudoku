a814edcb2b4682e6db894ad2e46cd57fad47a9b7
TheOGChips <swindell.christian.g@gmail.com>
2023-02-01 13:36:16 -0600

    Save file can now be selected properly

--- src/SavedGameMenu.cpp
+++ src/SavedGameMenu.cpp
@@ -1,22 +1,48 @@
 #include <ncurses.h>
 #include "SavedGameMenu.hpp"
+#include "colors.hpp"
 
 using namespace std;
 
 void SavedGameMenu::display_menu (const uint8_t Y_EDGE, const uint8_t X_EDGE, const options) {
     uint8_t display_line = Y_EDGE;
+    uint16_t input;
+    curs_set(false);
     clear();
     mvprintw(display_line++, X_EDGE, "Saved Games:");
+    //TODO: Add an edge case for saved games list being empty
     
     typedef list<string>::iterator list_iter;
-    for (list_iter iter = saved_games.begin(); iter != saved_games.end(); iter++) {
-        mvprintw(++display_line, X_EDGE, "%s", iter->c_str());
-    }
-    //TODO: Implement interactive menu somewhere in here
+    list_iter selection = saved_games.begin();
+    //uint8_t count = 0;
+    do {
+        static const uint8_t LIST_DISPLAY_START = display_line;
+        
+        for (list_iter iter = saved_games.begin(); iter != saved_games.end(); iter++) {
+            if (*selection == *iter) attron(COLOR_PAIR(MENU_SELECTION));
+            mvprintw(++display_line, X_EDGE, "%s", iter->c_str());
+            if (*selection == *iter) attroff(COLOR_PAIR(MENU_SELECTION));
+        }
+        refresh();
+        
+        input = getch();
+        if (input == KEY_DOWN and *selection != saved_games.back()) {
+            selection++;
+            display_line = LIST_DISPLAY_START;
+        }
+        else if (input == KEY_UP and *selection != saved_games.front()) {
+            selection--;
+            display_line = LIST_DISPLAY_START;
+        }
+        else if (input != KEY_ENTER) display_line = LIST_DISPLAY_START;
+    } while (input != KEY_ENTER);
     
+    //TODO: Try to split this function up into select and display functions
+    mvprintw(++display_line, X_EDGE, "You selected %s", selection->c_str());
     mvprintw(++display_line, X_EDGE, "Press ENTER to continue...");
     refresh();
     while (getch() != KEY_ENTER);
+    curs_set(true);
 }
 
 void SavedGameMenu::generate_saved_games_list () {
@@ -29,7 +55,15 @@ void SavedGameMenu::generate_saved_games_list () {
         }
     }
 }
-
+/*
+void SavedGameMenu::input () {
+    uint16_t input = getch();
+    switch (input) {
+        case KEY_DOWN:
+        case KEY_UP:
+    }
+}
+*/
 options SavedGameMenu::menu () {
     generate_saved_games_list();
     display_menu(TOP_PADDING, LEFT_PADDING, options::NONE);
