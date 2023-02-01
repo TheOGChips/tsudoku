766e264aa2788f770c03c616d683dcad0834326e
TheOGChips <swindell.christian.g@gmail.com>
2023-02-01 14:50:38 -0600

    Separated display_menu into select and display functions

--- include/SavedGameMenu.hpp
+++ include/SavedGameMenu.hpp
@@ -9,9 +9,13 @@
 
 class SavedGameMenu : public Menu {
     private:
+        typedef std::list<std::string>::iterator list_iter;
         std::list<std::string> saved_games;
+        list_iter selection;
+        
         void display_menu (const uint8_t, const uint8_t, const options) override;
         void generate_saved_games_list ();
+        void select_saved_game ();
         
     public:
         SavedGameMenu () {} //TODO: Not having this defined in SavedGameMenu.cpp caused a weird crash before. I suspected it might have been some odd linking issue, and compiling from scratch seems to have fixed it. Delete this comment later if no other problems occur.
--- src/SavedGameMenu.cpp
+++ src/SavedGameMenu.cpp
@@ -6,44 +6,14 @@ using namespace std;
 
 void SavedGameMenu::display_menu (const uint8_t Y_EDGE, const uint8_t X_EDGE, const options) {
     uint8_t display_line = Y_EDGE;
-    curs_set(false);
     clear();
-    if (saved_games.empty()) mvprintw(display_line++, X_EDGE, "You have no saved games.");
-    else {
-        mvprintw(display_line++, X_EDGE, "Saved Games:");
-        
-        typedef list<string>::iterator list_iter;
-        list_iter selection = saved_games.begin();
-        uint16_t input;
-        do {
-            static const uint8_t LIST_DISPLAY_START = display_line;
-            
-            for (list_iter iter = saved_games.begin(); iter != saved_games.end(); iter++) {
-                if (*selection == *iter) attron(COLOR_PAIR(MENU_SELECTION));
-                mvprintw(++display_line, X_EDGE, "%s", iter->c_str());
-                if (*selection == *iter) attroff(COLOR_PAIR(MENU_SELECTION));
-            }
-            refresh();
-            
-            input = getch();
-            if (input == KEY_DOWN and *selection != saved_games.back()) {
-                selection++;
-                display_line = LIST_DISPLAY_START;
-            }
-            else if (input == KEY_UP and *selection != saved_games.front()) {
-                selection--;
-                display_line = LIST_DISPLAY_START;
-            }
-            else if (input != KEY_ENTER) display_line = LIST_DISPLAY_START;
-        } while (input != KEY_ENTER);
-        
-        //TODO: Try to split this function up into select and display functions
-        mvprintw(++display_line, X_EDGE, "You selected %s", selection->c_str());
+    mvprintw(display_line++, X_EDGE, "Saved Games:");
+    for (list_iter iter = saved_games.begin(); iter != saved_games.end(); iter++) {
+        if (*selection == *iter) attron(COLOR_PAIR(MENU_SELECTION));
+        mvprintw(++display_line, X_EDGE, "%s", iter->c_str());
+        if (*selection == *iter) attroff(COLOR_PAIR(MENU_SELECTION));
     }
-    mvprintw(++display_line, X_EDGE, "Press ENTER to continue...");
     refresh();
-    while (getch() != KEY_ENTER);
-    curs_set(true);
 }
 
 void SavedGameMenu::generate_saved_games_list () {
@@ -55,18 +25,34 @@ void SavedGameMenu::generate_saved_games_list () {
             saved_games.push_back(iter->path().stem().string());
         }
     }
+    //TODO: Sort the list
 }
-/*
-void SavedGameMenu::input () {
-    uint16_t input = getch();
-    switch (input) {
-        case KEY_DOWN:
-        case KEY_UP:
+
+void SavedGameMenu::select_saved_game () {
+    uint16_t input,
+             size_offset;
+    selection = saved_games.begin();
+    
+    curs_set(false);
+    if (saved_games.empty()) mvprintw(TOP_PADDING, LEFT_PADDING, "You have no saved games.");
+    else {
+        do {
+            display_menu(TOP_PADDING, LEFT_PADDING, options::NONE);
+            
+            input = getch();
+            if (input == KEY_DOWN and *selection != saved_games.back()) selection++;
+            else if (input == KEY_UP and *selection != saved_games.front()) selection--;
+        } while (input != KEY_ENTER);
+        mvprintw(TOP_PADDING + saved_games.size() + 2, LEFT_PADDING, "You selected %s", selection->c_str());
     }
+    mvprintw(TOP_PADDING + saved_games.size() + 3, LEFT_PADDING, "Press ENTER to continue...");
+    refresh();
+    while (getch() != KEY_ENTER);
+    curs_set(true);
 }
-*/
+
 options SavedGameMenu::menu () {
     generate_saved_games_list();
-    display_menu(TOP_PADDING, LEFT_PADDING, options::NONE);
+    select_saved_game();
     return options::NONE;
 }
