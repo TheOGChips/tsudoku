e6524ef88a4ce3421ca38b9cb3181fff4b0fa876
TheOGChips <swindell.christian.g@gmail.com>
2023-01-31 21:44:51 -0600

    List of saved games now appears on screen

--- include/SavedGameMenu.hpp
+++ include/SavedGameMenu.hpp
@@ -2,16 +2,19 @@
 #define SAVEDGAMEMENU_HPP
 
 #include "Menu.hpp"
-//#include <filesystem>
+#include <filesystem>
+#include <list>
 
 //enum class options { NONE };
 
 class SavedGameMenu : public Menu {
     private:
+        std::list<std::string> saved_games;
         void display_menu (const uint8_t, const uint8_t, const options) override;
+        void generate_saved_games_list ();
         
     public:
-        SavedGameMenu () {}
+        SavedGameMenu () {} //TODO: Not having this defined in SavedGameMenu.cpp caused a weird crash before. I suspected it might have been some odd linking issue, and compiling from scratch seems to have fixed it. Delete this comment later if no other problems occur.
         ~SavedGameMenu () {}
         options menu () override;
 };
--- src/SavedGameMenu.cpp
+++ src/SavedGameMenu.cpp
@@ -1,17 +1,33 @@
 #include <ncurses.h>
 #include "SavedGameMenu.hpp"
 
+using namespace std;
+
 void SavedGameMenu::display_menu (const uint8_t Y_EDGE, const uint8_t X_EDGE, const options) {
     uint8_t display_line = Y_EDGE;
     clear();
     mvprintw(display_line++, X_EDGE, "Saved Games:");
+    
+    for (list<string>::iterator iter = saved_games.begin(); iter != saved_games.end(); iter++) {
+        mvprintw(++display_line, X_EDGE, "%s", iter->c_str());
+    }
     
     mvprintw(++display_line, X_EDGE, "Press ENTER to continue...");
     refresh();
     while (getch() != KEY_ENTER);
 }
 
+void SavedGameMenu::generate_saved_games_list () {
+    using namespace filesystem;
+    directory_iterator dir_iter(DIR);
+    for (; dir_iter != end(directory_iterator()); dir_iter++) {
+        saved_games.push_back(dir_iter->path().stem().string());
+    }
+    //TODO: Remove "completed_puzzles.txt" from the list
+}
+
 options SavedGameMenu::menu () {
+    generate_saved_games_list();
     display_menu(TOP_PADDING, LEFT_PADDING, options::NONE);
     return options::NONE;
 }
