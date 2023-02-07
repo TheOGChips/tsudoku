dba665498afcdb3e2862dfba4a0daf1dfde8934f
TheOGChips <swindell.christian.g@gmail.com>
2023-02-07 02:27:53 -0600

    Added notes to colors.hpp

--- include/colors.hpp
+++ include/colors.hpp
@@ -3,6 +3,12 @@
 
 #include <string>   //std::string
 
+/* NOTE: This file contains constants used for identifying NCurses color codes.
+ * 
+ *  colors -> enum constants used for identifying NCurses COLOR_PAIRs when printing to the screen
+ *  color_code -> array used to identify NCurses COLOR_PAIRs when writing/reading a saved game to
+ *                from a file
+ */
 enum colors { UNKNOWN = 1, GIVEN, CANDIDATES_Y, CANDIDATES_B, GUESS, MENU_SELECTION };
 const std::string color_code[6] = { "n", "u", "r", "y", "b", "g" };
 
