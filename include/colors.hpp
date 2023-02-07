b46ca2327757ed2c54c4eab9f0cf1a13200f24f9
TheOGChips <swindell.christian.g@gmail.com>
2023-02-06 23:57:56 -0600

    Fixed formatting for colors.hpp

--- include/colors.hpp
+++ include/colors.hpp
@@ -3,7 +3,7 @@
 
 #include <string>   //std::string
 
-enum colors { UNKNOWN = 1, GIVEN, CANDIDATES_Y, CANDIDATES_B, GUESS, MENU_SELECTION};
-const std::string color_code[6] = { "n", "u", "r", "y", "b", "g"};
+enum colors { UNKNOWN = 1, GIVEN, CANDIDATES_Y, CANDIDATES_B, GUESS, MENU_SELECTION };
+const std::string color_code[6] = { "n", "u", "r", "y", "b", "g" };
 
 #endif
