9d6d28d503dd9951d0219d6860c56a3fc912a0ae
TheOGChips <swindell.christian.g@gmail.com>
2023-02-21 05:56:13 -0600

    Added notes and fixed formatting for MainMenu.cpp

--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -6,6 +6,11 @@
 
 using namespace std;
 
+/* NOTE:
+ * Name: Class Constructor
+ * Purpose: Initializes the NCurses environment and global NCurses settings.
+ * Parameters: None
+ */
 MainMenu::MainMenu () {
     initscr();  //TODO: Account for already having been called somehow
     cbreak();   //TODO: Will need to account for signal handling
@@ -16,12 +21,28 @@ MainMenu::MainMenu () {
     init_pair(MENU_SELECTION, COLOR_BLACK, COLOR_WHITE);
 }
 
+/* NOTE:
+ * Name: Class Destructor
+ * Purpose: Unsets the NCurses environment once the user chooses to exit the program.
+ * Parameters: None
+ */
 MainMenu::~MainMenu () {
     echo();
     nocbreak();
     endwin();
 }
 
+/* NOTE:
+ * Name: display_menu (pure virtual override)
+ * Purpose: Displays the main menu. The currently selected option is always highlighted. The main
+ *          menu is re-rendered each time the user uses the Up/Down keys to highlight a different
+ *          option.
+ * Parameters:
+ *      TODO: Switch to using a cell object instead of Y, X
+ *      Y -> Starting display line for main menu.
+ *      X -> Starting display column for main menu.
+ *      OPT -> The currently selected main menu option.
+ */
 void MainMenu::display_menu (const uint8_t Y, const uint8_t X, const options OPT) {
     const string TITLE = "MAIN MENU";
     const uint8_t Y_CENTER = Y / 2,
@@ -43,19 +64,46 @@ void MainMenu::display_menu (const uint8_t Y, const uint8_t X, const options OPT
     refresh();
 }
 
+/* NOTE:
+ * Name: set_VERTICAL_DIVIDER
+ * Purpose: Sets the value of the vertical divider between the display puzzle and the in-game menu.
+ * Parameters:
+ *      VAL -> The number of columns to be used as the vertical divider. If the in-game menu is
+ *             disabled, the vertical divider is set to 0.
+ */
 void MainMenu::set_VERTICAL_DIVIDER (const uint8_t VAL) {
     VERTICAL_DIVIDER = VAL;
 }
 
+/* NOTE:
+ * Name: set_IN_GAME_MENU_DISPLAY_SPACING
+ * Purpose: Sets the number of columns used to display the in-game menu.
+ * Parameters:
+ *      VAL -> The number of columns to be used to display the in-game menu. If the in-game menu is
+ *             disabled, the number of columns is set to 0.
+ */
 void MainMenu::set_IN_GAME_MENU_DISPLAY_SPACING (const uint8_t VAL) {
     IN_GAME_MENU_DISPLAY_SPACING = VAL;
 }
 
+/* NOTE:
+ * Name: set_WINDOW_XMIN
+ * Purpose: Sets the minimum number of columns the terminal window must be in order to play. This
+ *          size is dependent on whether the in-game menu is enabled.
+ * Parameters: None
+ */
 void MainMenu::set_WINDOW_XMIN () {
     WINDOW_XMIN = LEFT_PADDING + PUZZLE_SPACE + VERTICAL_DIVIDER + IN_GAME_MENU_DISPLAY_SPACING +
                   RIGHT_PADDING;
 }
 
+/* NOTE:
+ * Name: menu (overload)
+ * Purpose: Sets class-level global variables before running the actual main menu.
+ * Parameters:
+ *      USE_IN_GAME_MENU -> Boolean indicating whether the in-game menu is disabled or not. This
+ *                          affects the enforced size of the terminal window.
+ */
 options MainMenu::menu (const bool USE_IN_GAME_MENU) {
     if (not USE_IN_GAME_MENU) {
         set_VERTICAL_DIVIDER(0);
@@ -66,22 +114,35 @@ options MainMenu::menu (const bool USE_IN_GAME_MENU) {
     return menu();
 }
 
+/* NOTE:
+ * Name: menu (pure virtual override)
+ * Purpose: On the first call, determines whether the terminal window meets the size requirments. If
+ *          not, the program does not proceed until the user resizes the terminal window
+ *          appropriately. Afterwards and on subsequent calls, displays the main menu.
+ * Parameters: None
+ */
 options MainMenu::menu () {
     uint8_t x_max,
             y_max;
     static bool first_pass = true;
     getmaxyx(stdscr, y_max, x_max);
-    curs_set(false);
+    curs_set(false);    //NOTE: Turn the cursor off while in the main menu.
 
+    /* NOTE: Enforce window size on initial startup if terminal window is not already compliant. The
+     *       user will be updated as to whether the window is the correct size or not after pressing
+     *       the Enter key twice. The reason the Enter key must be hit twice is actually a bug I
+     *       decided to make a feature. For some reason, it's required to hit twice only in this
+     *       section. Since it doesn't affect anything else, I just left it alone.
+     */
     if (first_pass) {
         while (y_max < WINDOW_YMIN or x_max < WINDOW_XMIN) {
             uint8_t x_curr,
                     y_curr;
             ::clear();
             string msg1 = "The current window is too small",
-                msg4 = "Resize the terminal window and press Enter twice to continue";
+                   msg4 = "Resize the terminal window and press Enter twice to continue";
             stringstream msg2,
-                        msg3;
+                         msg3;
             msg2 << "Required dimensions: " << WINDOW_XMIN+0 << " x " << WINDOW_YMIN+0;
             msg3 << "Current dimensions: " << x_max+0 << " x " << y_max+0;
             mvprintw(y_max/2,     x_max/2 - msg1.size()/2,       "%s", msg1.c_str());
@@ -90,8 +151,8 @@ options MainMenu::menu () {
             mvprintw(y_max/2 + 5, x_max/2 - msg4.size()/2,       "%s", msg4.c_str());
             refresh();
             getmaxyx(stdscr, y_max, x_max);
-            while (getch() != KEY_ENTER);   //NOTE: For some reason, the Enter key needs to be pressed
-        }                                   //      twice here
+            while (getch() != KEY_ENTER);   //NOTE: For some reason, the Enter key needs to be
+        }                                   //      pressed twice here
         ::clear();
         
         string msg1 = "The window is now an appropriate size",
@@ -104,58 +165,80 @@ options MainMenu::menu () {
     }
     ::clear();
     
+    //NOTE: Display the main menu starting with the "New Game" option highlighted
     options opt = options::NEW_GAME;
     display_menu(y_max, x_max, opt);
     
+    //NOTE: Cycle through the menu until the user selects an option
     uint16_t input;
     do {
         input = getch();
         switch (input) {
-            case KEY_DOWN:  ++opt;
-                            display_menu(y_max, x_max, opt);
-                            break;
+            case KEY_DOWN:
+                ++opt;
+                display_menu(y_max, x_max, opt);
+                break;
                             
-            case KEY_UP:    --opt;
-                            display_menu(y_max, x_max, opt);
-                            break;
+            case KEY_UP:
+                --opt;
+                display_menu(y_max, x_max, opt);
+                break;
                 
             default:;
         }
     } while (input != KEY_ENTER);
     
     ::clear();
-    curs_set(true);
+    curs_set(true); //NOTE: Turn the cursor back on before leaving the main menu
     return opt;
 }
 
+/* NOTE:
+ * Name: Operator ++ (pre-increment, overloaded)
+ * Purpose: Shorthand convenience for changing main menu options.
+ * Parameters:
+ *      opt -> The previously selected main menu option to update.
+ */
 options operator ++ (options& opt) {
     //return opt = (opt == options::NEW_GAME) ? options::RESUME_GAME : options::SHOW_STATS;
     switch (opt) {
-        case options::NEW_GAME: opt = options::RESUME_GAME;
-                                     break;
+        case options::NEW_GAME:
+            opt = options::RESUME_GAME;
+            break;
                                      
-        case options::RESUME_GAME: opt = options::SHOW_STATS;
-                                       break;
+        case options::RESUME_GAME:
+            opt = options::SHOW_STATS;
+            break;
                                        
-        case options::SHOW_STATS: opt = options::EXIT;
-                                     break;
+        case options::SHOW_STATS:
+            opt = options::EXIT;
+            break;
                                      
         default: opt = options::EXIT;
     }
     return opt;
 }
 
+/* NOTE:
+ * Name: Operator -- (pre-decrement, overloaded)
+ * Purpose: Shorthand convenience for changing main menu options.
+ * Parameters:
+ *      opt -> The previously selected main menu option to update.
+ */
 options operator -- (options& opt) {
     //return opt = (opt == options::SHOW_STATS) ? options::RESUME_GAME : options::NEW_GAME;
     switch (opt) {
-        case options::EXIT: opt = options::SHOW_STATS;
-                                     break;
+        case options::EXIT:
+            opt = options::SHOW_STATS;
+            break;
                                      
-        case options::SHOW_STATS: opt = options::RESUME_GAME;
-                                       break;
+        case options::SHOW_STATS:
+            opt = options::RESUME_GAME;
+            break;
                                        
-        case options::RESUME_GAME: opt = options::NEW_GAME;
-                                     break;
+        case options::RESUME_GAME:
+            opt = options::NEW_GAME;
+            break;
                                      
         default: opt = options::NEW_GAME;
     }
