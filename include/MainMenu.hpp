fd805397ec2d82973559f33bee6fab6656b271ac
TheOGChips <swindell.christian.g@gmail.com>
2023-02-21 05:14:49 -0600

    Added notes to MainMenu.hpp

--- include/MainMenu.hpp
+++ include/MainMenu.hpp
@@ -5,11 +5,28 @@
 
 //enum class options { NEW_GAME, RESUME_GAME, SHOW_STATS };   //TODO: Add EXIT option
 
+/* NOTE:
+ * Class: MainMenu
+ * Purpose: Menu-derived class that displays and controls the main menu the user sees before and
+ *          after every game.
+ * Private variables:
+ *      BOTTOM_PADDING -> Empty space between the bottom of the terminal window and the in-terminal
+ *                        display.
+ *      RIGHT_PADDING -> Empty space between the right side of the terminal window and the in
+ *                       terminal display.
+ *      RESULT_MSG_SPACE -> Minimum number of lines required to properly display the puzzle's
+ *                          evaluation result.
+ *      WINDOW_YMIN -> Minimum terminal window lines enforced on startup. If the window does not meet
+ *                     this requirement, it will halt until the user resizes their terminal window.
+ *      WINDOW_XMIN -> Minimum terminal window columns enforced on startup. This value is dependent
+ *                     on whether the in-game menu is enable. If the window does not meet this
+ *                     requirement, it will halt until the user resizes their terminal window.
+ */
 class MainMenu : public Menu {
     private:
-        const uint8_t BOTTOM_PADDING = TOP_PADDING, //blank space on all the edges of the window
+        const uint8_t BOTTOM_PADDING = TOP_PADDING,
                       RIGHT_PADDING = LEFT_PADDING,
-                      RESULT_MSG_SPACE = 3,     //space for the evaluation result to appear
+                      RESULT_MSG_SPACE = 3,
                       WINDOW_YMIN = TOP_PADDING + PUZZLE_SPACE + RESULT_MSG_SPACE + BOTTOM_PADDING;
         uint8_t WINDOW_XMIN;
         
@@ -19,14 +36,15 @@ class MainMenu : public Menu {
         void set_WINDOW_XMIN ();
         
     public:        
-        MainMenu ();
-        ~MainMenu ();
+        MainMenu ();    //NOTE: Default constructor. Does nothing.
+        ~MainMenu ();   //NOTE: Default destructor. Does nothing.
         options menu () override;
         options menu (const bool);
         //friend options operator ++ (options&);
         //friend options operator -- (options&);
 };
 
+//TODO: See if you can get rid of having these declarations here.
 options operator ++ (options&);
 options operator -- (options&);
 
