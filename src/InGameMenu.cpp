9f0351eea6c779672d083529196bd46ac8b1fee2
TheOGChips <swindell.christian.g@gmail.com>
2023-02-22 02:10:44 -0600

    Added notes to InGameMenu.cpp

--- src/InGameMenu.cpp
+++ src/InGameMenu.cpp
@@ -1,18 +1,35 @@
 #include <ncurses.h>
 #include "InGameMenu.hpp"
 #include "colors.hpp"
-#include <map>
-#include <fstream>      //std::ofstream, std::ifstream
+#include <map>      //std::map
+#include <fstream>  //std::ofstream, std::ifstream
 
 using namespace std;
 
 //TODO: This might need to be dynamically allocated instead
+/* NOTE:
+ * Name: Class Constructor
+ * Purpose: Initializes the display matrix, so the in-game menu can track any changes made during
+ *          gameplay.
+ * Parameters:
+ *      display_matrix -> The display matrix from the main gameplay loop in Sudoku.
+ */
 InGameMenu::InGameMenu (uint8_t display_matrix[DISPLAY_MATRIX_ROWS][DISPLAY_MATRIX_COLUMNS]) {
     for (uint8_t i = 0; i < DISPLAY_MATRIX_COLUMNS; i++) {
         this->display_matrix[i] = display_matrix[i];
     }
 }
 
+/* NOTE:
+ * Name: display_menu (pure virtual override)
+ * Purpose: Displays the in-game menu. The currently selected option is always highlighted. The
+ *          in-game menu is re-rendered each time the user uses the Up/Down keys to highlight a
+ *          different option.
+ * Parameters:
+ *      EDGE -> Starting cell the in-game menu will display at. The menu title should display on the
+ *              line below the top padding and the column after the vertical divider.
+ *      OPT -> The currently highlighted main menu option.
+ */
 void InGameMenu::display_menu (const cell EDGE, const options OPT) {
     const uint8_t NUM_OPTS = 3;
     const string TITLE = "IN-GAME MENU",
@@ -33,6 +50,15 @@ void InGameMenu::display_menu (const cell EDGE, const options OPT) {
     refresh();
 }
 
+/* NOTE:
+ * Name: clear (NCurses library function overload)
+ * Purpose: Clears the in-game menu display area.
+ * Parameters:
+ *      Y_EDGE -> Line to start clearing from.
+ *      X_EDGE -> Column to start clearing from.
+ * 
+ * TODO: Use a cell here instead?
+ */
 void InGameMenu::clear (const uint8_t Y_EDGE, const uint8_t X_EDGE) {
     for (uint8_t y = Y_EDGE + IN_GAME_MENU_TITLE_SPACING + NUM_OPTS + 2; y < getmaxy(stdscr); y++) {
         move(y, X_EDGE);
@@ -40,6 +66,15 @@ void InGameMenu::clear (const uint8_t Y_EDGE, const uint8_t X_EDGE) {
     }
 }
 
+/* NOTE:
+ * Name: display_rules
+ * Purpose: Displays the rules of sudoku.
+ * Parameters:
+ *      Y_EDGE -> Line to start the display at.
+ *      X_EDGE -> Column to start the display at.
+ * 
+ * TODO: Use a cell here instead?
+ */
 void InGameMenu::display_rules (const uint8_t Y_EDGE, const uint8_t X_EDGE) {
     const string TITLE = "RULES FOR PLAYING SUDOKU",
                  RULES_INTRO = string("Sudoku is a puzzle game using the numbers 1-9. The ") +
@@ -53,7 +88,7 @@ void InGameMenu::display_rules (const uint8_t Y_EDGE, const uint8_t X_EDGE) {
                  
     const uint8_t NUM_RULES = 4;
     string rules_text[NUM_RULES] = { RULES_INTRO, RULES_ROWS, RULES_COLUMNS, RULES_SUBMATRIX };
-    uint8_t display_offset = NUM_OPTS + 2;
+    uint8_t display_offset = NUM_OPTS + 2; //NOTE: Offset to allow the display to start below the list of menu options
     
     mvprintw(Y_EDGE + IN_GAME_MENU_TITLE_SPACING + display_offset++, X_EDGE, "%s", TITLE.c_str());
     for (uint8_t i = 0; i < NUM_RULES; i++) {
@@ -62,6 +97,15 @@ void InGameMenu::display_rules (const uint8_t Y_EDGE, const uint8_t X_EDGE) {
     }
 }
 
+/* NOTE:
+ * Name: display_manual
+ * Purpose: Displays the tsudoku game manual.
+ * Parameters:
+ *      Y_EDGE -> Line to start the display at.
+ *      X_EDGE -> Column to start the display at.
+ * 
+ * TODO: Use a cell here instead?
+ */
 void InGameMenu::display_manual (const uint8_t Y_EDGE, const uint8_t X_EDGE) {
     const string TITLE = "TSUDOKU GAME MANUAL",
                  MANUAL_INTRO = string("Red numbers are givens provided for you when the puzzle ") +
@@ -94,7 +138,7 @@ void InGameMenu::display_manual (const uint8_t Y_EDGE, const uint8_t X_EDGE) {
     const uint8_t NUM_MANUAL = 6;
     string manual_text[NUM_MANUAL] = { MANUAL_INTRO, MANUAL_M, MANUAL_Q, MANUAL_DIR_KEYS,
                                        MANUAL_NUM, MANUAL_ENTER };
-    uint8_t display_offset = NUM_OPTS + 2;
+    uint8_t display_offset = NUM_OPTS + 2;  //NOTE: Offset to allow the display to start below the list of menu options
     
     mvprintw(Y_EDGE + IN_GAME_MENU_TITLE_SPACING + display_offset++, X_EDGE, "%s", TITLE.c_str());
     for (uint8_t i = 0; i < NUM_MANUAL; i++) {
@@ -103,23 +147,45 @@ void InGameMenu::display_manual (const uint8_t Y_EDGE, const uint8_t X_EDGE) {
     }
 }
 
+/* NOTE:
+ * Name: screen_reader
+ * Purpose: Prints string of text to the screen in a nicely formatted way that makes it easy to
+ *          read. This function parses the input string by spaces and determines when to start
+ *          printing on a new line (i.e. if adding another word would overlap with the window border
+ *          and ruin the sudoku board display).
+ * Parameters:
+ *      Y_EDGE -> Line to start the display at.
+ *      X_EDGE -> Column to start the display at.
+ *      str -> Input string to be printed in the in-game menu's display area.
+ *      display_offset -> Line offset to allow displaying correctly below the in-game menu title and
+ *                        options.
+ * 
+ * TODO: Use a cell here instead?
+ */
 void InGameMenu::screen_reader (const uint8_t Y_EDGE, const uint8_t X_EDGE, string str,
                                 uint8_t& display_offset) {
     string display_str;
     while (not str.empty()) {
-        size_t space_pos = str.find_first_of(' ');
+        size_t space_pos = str.find_first_of(' ');  //NOTE: Index of the next space character in str
         
+        //NOTE: If the end of the string has been reached, print what remains
         if (space_pos == string::npos) {
             display_str += str;
             str.clear();
             mvprintw(Y_EDGE + IN_GAME_MENU_TITLE_SPACING + display_offset++, X_EDGE, "%s",
                      display_str.c_str());
         }
+        
+        //NOTE: If there is enough space to display the next word, add it to the display string
         else if (display_str.size() + space_pos + 1 < IN_GAME_MENU_DISPLAY_SPACING) {
             display_str += str.substr(0, space_pos + 1);
             str = str.substr(space_pos + 1);
         }
-        else {
+        
+        /* NOTE: Otherwise, print the display string on the current line, clear the string, and 
+         *       increment the display offset.
+         */
+        else {  
             mvprintw(Y_EDGE + IN_GAME_MENU_TITLE_SPACING + display_offset++, X_EDGE, "%s",
                      display_str.c_str());
             display_str.clear();
@@ -128,6 +194,16 @@ void InGameMenu::screen_reader (const uint8_t Y_EDGE, const uint8_t X_EDGE, stri
     display_str.clear();
 }
 
+/* NOTE:
+ * Name: save_game
+ * Purpose: Prompts the user for the name to save the game under before saving the game. Displays a
+ *          success message after having saved.
+ * Parameters:
+ *      Y_EDGE -> Line to start the display at. The prompt for the save file name will appear here.
+ *      X_EDGE -> Column to start the display at. The prompt for the save file name will start here.
+ * 
+ * TODO: Use a cell here instead?
+ */
 void InGameMenu::save_game (const uint8_t Y_EDGE, const uint8_t X_EDGE) {
     uint8_t display_offset = NUM_OPTS + 2;
     mvprintw(Y_EDGE + IN_GAME_MENU_TITLE_SPACING + display_offset++, X_EDGE, "Enter save file name: ");
@@ -138,6 +214,14 @@ void InGameMenu::save_game (const uint8_t Y_EDGE, const uint8_t X_EDGE) {
     curs_set(false);
 }
 
+/* NOTE:
+ * Name: save_game (static overload)
+ * Purpose: Performs the actual saving of the current game to a CSV file in $HOME/.tsudoku. Each
+ *          cell is saved as its integer representation follow by a letter representing the
+ *          foreground color of the character.
+ * Parameters:
+ *      display_matrix -> The display matrix as seen in the terminal window.
+ */
 string InGameMenu::save_game (uint8_t* display_matrix[DISPLAY_MATRIX_COLUMNS]) {
     const uint8_t NAME_SIZE = 18;   //NOTE: NAME_SIZE limited by window width requirements of no
     char name[NAME_SIZE];           //      in-game menu mode
@@ -179,8 +263,13 @@ string InGameMenu::save_game (uint8_t* display_matrix[DISPLAY_MATRIX_COLUMNS]) {
     return string(name);
 }
 
+/* NOTE:
+ * Name: menu (overload)
+ * Purpose: Controls the menu display based on the option chosen by the user.
+ * Parameters: None
+ */
 options InGameMenu::menu () {    
-    curs_set(false);
+    curs_set(false);    //NOTE: Turn off the cursor while in the in-game menu
     options opt = options::RULES;
     uint16_t input;
     do {
@@ -219,15 +308,27 @@ options InGameMenu::menu () {
     } while (tolower(input) != 'm');
     opt = options::NONE;
     display_menu(cell {TOP_PADDING, IN_GAME_MENU_LEFT_EDGE}, opt);
-    curs_set(true);
+    curs_set(true); //NOTE: Turn the cursor back on before returning to the game
     
     return opt;
 }
 
+/* NOTE:
+ * Name: Operator ++ (post-increment, overloaded)
+ * Purpose: Shorthand convenience for changing in-game menu options.
+ * Parameters:
+ *      opt -> The previously highlighted in-game menu option to update.
+ */
 options operator ++ (options& opt, int) {
     return opt = (opt == options::RULES) ? options::MANUAL : options::SAVE_GAME;
 }
 
+/* NOTE:
+ * Name: Operator -- (post-decrement, overloaded)
+ * Purpose: Shorthand convenience for changing in-game menu options.
+ * Parameters:
+ *      opt -> The previously highlighted in-game option to update.
+ */
 options operator -- (options& opt, int) {
     return opt = (opt == options::SAVE_GAME) ? options::MANUAL : options::RULES;
 }
