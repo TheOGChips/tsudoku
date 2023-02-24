7b807d06218c950075eb0f5d03ec465d3fff74d5
TheOGChips <swindell.christian.g@gmail.com>
2023-02-23 21:43:51 -0600

    Games will now automatically reset on detecting the correct window size instead of requiring the player to press Enter twice

--- src/misc.cpp
+++ src/misc.cpp
@@ -1,6 +1,8 @@
 #include <ncurses.h>
 #include "misc.hpp"
 #include <sstream>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
@@ -18,38 +20,42 @@ cell WINDOW_REQ;    //NOTE: The size requirements for the terminal window.
  * Parameters: None
  */
 bool invalid_window_size_handler () {
-    uint8_t x_max,
-            y_max;
+    uint8_t y_max,
+            x_max;
     getmaxyx(stdscr, y_max, x_max);
-    if (y_max != WINDOW_REQ.first or x_max != WINDOW_REQ.second) {
+    
+    if (y_max == WINDOW_REQ.first and x_max == WINDOW_REQ.second) {
+        return false;
+    }
+    else {
         do {
             clear();
             string msg1 = "The current window is too small",
                    msg4 = "Resize the terminal window and press Enter twice to continue";
             stringstream msg2,
                          msg3;
-            msg2 << "Required dimensions: " << WINDOW_REQ.second+0 << " x " << WINDOW_REQ.first+0;
+            msg2 << "Required dimensions: " << WINDOW_REQ.second+0 << " x "
+                                            << WINDOW_REQ.first+0;
             msg3 << "Current dimensions: " << x_max+0 << " x " << y_max+0;
             mvprintw(y_max/2,     x_max/2 - msg1.size()/2,       "%s", msg1.c_str());
             mvprintw(y_max/2 + 2, x_max/2 - msg2.str().size()/2, "%s", msg2.str().c_str());
             mvprintw(y_max/2 + 3, x_max/2 - msg3.str().size()/2, "%s", msg3.str().c_str());
             
             if (msg4.size() > x_max) {
-                 const uint8_t PARTITION = 30;
-                 mvprintw(y_max/2 + 5, x_max/2 - msg4.substr(0, PARTITION).size()/2,
-                          "%s", msg4.substr(0, PARTITION).c_str());
-                 mvprintw(y_max/2 + 6, x_max/2 - msg4.substr(PARTITION).size()/2,
-                          "%s", msg4.substr(PARTITION).c_str());
+                const uint8_t PARTITION = 30;
+                mvprintw(y_max/2 + 5, x_max/2 - msg4.substr(0, PARTITION).size()/2,
+                         "%s", msg4.substr(0, PARTITION).c_str());
+                mvprintw(y_max/2 + 6, x_max/2 - msg4.substr(PARTITION).size()/2,
+                         "%s", msg4.substr(PARTITION).c_str());
             }
             else mvprintw(y_max/2 + 5, x_max/2 - msg4.size()/2,  "%s", msg4.c_str());
             
             refresh();
+            this_thread::sleep_for(chrono::milliseconds(100));
             getmaxyx(stdscr, y_max, x_max);
-            while (getch() != KEY_ENTER);   //NOTE: For some reason, the Enter key needs to be
-        } while (y_max != WINDOW_REQ.first or x_max != WINDOW_REQ.second); //      pressed twice here
+        } while (y_max != WINDOW_REQ.first or x_max != WINDOW_REQ.second);
         clear();
         
         return true;
     }
-    else return false;
 }
