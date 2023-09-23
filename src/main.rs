d150d5cfb3ddfaf1f70a97551f1c376816a1d5e4
TheOGChips <swindell.christian.g@gmail.com>
2023-09-23 03:36:02 -0500

    Checks if >1 argument is passed

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,25 @@ fn main() {
         )
         .get_matches();
 
+    let mut num_clargs: u8 = 0; //NOTE: I'm surprised there isn't a builtin way to get this
     let use_in_game_menu: bool =
-        if matches.get_flag("no-in-game-menu") { true }
+        if matches.get_flag("no-in-game-menu") {
+            num_clargs += 1;
+            true
+        }
         else { false };
     let delete_saved_games: bool =
-        if matches.get_flag("delete-saved-games") { true }
+        if matches.get_flag("delete-saved-games") {
+            num_clargs += 1;
+            true
+        }
         else { false };
+
+    if num_clargs > 1 {
+        eprintln!("\nError: Too many arguments. Only one argument can be accepted.\n");
+        return;
+    }
     println!("use_in_game_menu: {}", use_in_game_menu);
     println!("delete_saved_games: {}", delete_saved_games);
+    //TODO: Print error message about invalid argument
 }
