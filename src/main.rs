a951ab404fec5d1ba5c8eb71c13927524ba46040
TheOGChips <swindell.christian.g@gmail.com>
2024-10-16 15:15:50 -0400

    Removed linter allow statements

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-// TODO: Get rid of these linter-related lines if you can
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
 //include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 use clap;
 use std::{
