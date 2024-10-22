c672cac392aa82b64fb83d84375336ba0dafc6d9
TheOGChips <swindell.christian.g@gmail.com>
2024-10-22 17:56:40 -0400

    Created batch file for WSL users as a clickable application.

--- tsudoku.bat
+++ tsudoku.bat
@@ -1,7 +1,36 @@
-# NOTE: You should always have one of the below lines commented, otherwise this
-#       will run tsudoku twice, which is probably not what you want.
-#       The first line runs tsudoku with the in-game menu enabled.
-#       The second line runs tsudoku with the in-game menu disabled.
+@echo off
+echo:
+echo Thank you for playing tsudoku!
 
-wsl.exe '~/.local/tsudoku'
-#wsl.exe '~/.local/tsudoku -n'
+:loop
+:: Prompt the player for a response
+echo:
+set /p input= Would you like the in-game menu enabled (y/n)? 
+
+:: If yes, play with in-game menu enabled
+if %input%==y (goto :enabled)
+if %input%==Y (goto :enabled)
+
+:: If no, play with in-game menu disabled
+if %input%==n (goto :disabled)
+if %input%==N (goto :disabled)
+
+:: If player input is invalid, notify them
+goto :try_again
+
+:enabled
+wsl.exe '~/.cargo/bin/tsudoku'
+goto :fin
+
+:disabled
+wsl.exe '~/.cargo/bin/tsudoku -n'
+goto :fin
+
+:: Notify the player of invalid input, then loop back up to get input again
+:try_again
+echo That is invalid input. Please try again...
+goto :loop
+
+:fin
+echo Thank you for playing tsudoku! This window will now close...
+timeout 5 > NUL
